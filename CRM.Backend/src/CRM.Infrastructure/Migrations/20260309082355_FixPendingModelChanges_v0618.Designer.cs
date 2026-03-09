199c634bf8a0931ef6e3f7aca116018f828f7ca3
Abhishek Lal <abhi.lal@gmail.com>
2026-03-09 09:25:44 +0100

    fix(migration): add FixPendingModelChanges_v0618 migration - captures model changes missing from InitialCreate (IsApiGroup, ApiKeys, AutomationRules, Events, Notifications, SavedViews tables)

--- CRM.Backend/src/CRM.Infrastructure/Migrations/20260309082355_FixPendingModelChanges_v0618.Designer.cs
+++ CRM.Backend/src/CRM.Infrastructure/Migrations/20260309082355_FixPendingModelChanges_v0618.Designer.cs
@@ -4,6 +4,7 @@ using CRM.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CRM.Infrastructure.Migrations
 {
     [DbContext(typeof(CrmDbContext))]
-    partial class CrmDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260309082355_FixPendingModelChanges_v0618")]
+    partial class FixPendingModelChanges_v0618
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
